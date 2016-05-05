b8d4b1101e95c66add037a9a31235a472cd05525
Matthias L. Jugel <matthias.jugel@streamdrill.com>
2016-05-05 22:31:33 +0200

    small caps

--- cmake/mmcau.cmake
+++ cmake/mmcau.cmake
@@ -1,7 +1,7 @@
 # MMCAU library
 
-INCLUDE_DIRECTORIES(${KSDK_ROOT}/middleware/mmcau_2.0.0)
-INCLUDE_DIRECTORIES(${KSDK_ROOT}/middleware/mmcau_2.0.0/asm-cm4-cm7/src)
+include_directories(${KSDK_ROOT}/middleware/mmcau_2.0.0)
+include_directories(${KSDK_ROOT}/middleware/mmcau_2.0.0/asm-cm4-cm7/src)
 add_library(mmcau STATIC
   "${KSDK_ROOT}/middleware/mmcau_2.0.0/asm-cm4-cm7/src/cau2_defines.hdr"
   "${KSDK_ROOT}/middleware/mmcau_2.0.0/asm-cm4-cm7/src/mmcau_aes_functions.s"
